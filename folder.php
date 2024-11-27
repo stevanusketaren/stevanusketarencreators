8b0b3df6a6d8519b6a78ffaa6cfd95b341f60bbe
stevanusketaren <etarenstevanusk@gmail.com>
2024-11-27 14:43:03 +0700

    Update folder.php

--- folder.php
+++ folder.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hari Belajar PHP</title>
+</head>
+<body>
+    <?php
+    for ($i = 1; $i <= 1000; $i++) {
+        echo "Ini adalah hari ke-$i belajar PHP<br>";
+    }
+    ?>
+</body>
+</html>
 
